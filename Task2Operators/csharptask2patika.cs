c6b95cd2e1a3ae2947b3d5bae9a0cb0a41aba677
Burak Kağan Köksal <koksalburakkagan@gmail.com>
2022-02-28 00:26:41 +0300

    Operators

---
+++ Task2Operators/csharptask2patika.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace csharptask2patika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atama ve işlemli atama
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            y += 2;
+            y /= 1;
+            x *= 2;
+
+            //Mantıksal Operatörler ||, &&, !
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+            {
+                Console.WriteLine("Perfect!");
+            }
+            else if (isSuccess || isCompleted)
+            {
+                Console.WriteLine("Great!");
+            }
+            else if (isSuccess && !isCompleted)
+            {
+                Console.WriteLine("Fine!");
+            }
+            //İlişkisel Operatörler <, >, <=, >=, ==, !=
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+            sonuc = a > b;
+            sonuc = a >= b;
+            sonuc = a <= b;
+            sonuc = a == b;
+            sonuc = a != b;
+
+            //Aritmetik Operatörler /, *, +, -
+            int sayi1 = 10;
+            int sayi2 = 5;
+
+            int sonuc1 = sayi1 / sayi2;
+            int sonuc2 = sayi1 * sayi2;
+            int sonuc3 = sayi1 + sayi2;
+            int sonuc4 = sayi1++;
+
+            // % mod
+            int sonuc5 = 20 % 3;
+        }
+    }
+}
